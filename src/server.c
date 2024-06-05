395e3462a575ac2437bf7e6717faec65558e9ac7
Christian Mancini <mancinichristian00@gmail.com>
2024-06-05 16:53:52 +0200

    Added kill signal handler
    Now we can close connection with CTRL^C and free the socket port
    
    Signed-off-by: Christian Mancini <mancinichristian00@gmail.com>

--- src/server.c
+++ src/server.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <signal.h>
 #include "../include/common.h"
 #include "../include/address_book.h"
 #include "../include/auth.h"
@@ -11,11 +12,14 @@ void handle_client(int client_sock);
 void process_command(int client_sock, Command *cmd);
 void send_response(int client_sock, const char *response);
 
+void signal_handler(int server_sock);
+
 int main()
 {
     int server_sock, client_sock;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
+
 
     // Create socket
     if ((server_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -42,8 +46,8 @@ int main()
         close(server_sock);
         exit(EXIT_FAILURE);
     }
-
-    printf("Server is running on port %d...\n", PORT);
+    signal(SIGINT, signal_handler);
+    printf("Server is running on port %d and pid %d ...\n", PORT, getpid());
 
     // Accept client connections
     while ((client_sock = accept(server_sock, (struct sockaddr *)&client_addr, &client_len)) > 0)
@@ -51,6 +55,13 @@ int main()
 
     close(server_sock);
     return 0;
+}
+
+void signal_handler(int server_sock) {
+    printf("\nSignal received, closing socket...\n");
+    close(server_sock);
+    printf("Done! :)\n");
+    exit(EXIT_SUCCESS);
 }
 
 void handle_client(int client_sock)
@@ -66,7 +77,12 @@ void process_command(int client_sock, Command *cmd)
 {
     printf("Received command: %d\n", cmd->type);
     printf("Received data: %s\n", cmd->data);
-    char response[BUFFER_SIZE] = {0};
+
+    printf("qui");
+    char response[BUFFER_SIZE] = "";
+
+    memset(response, '\0', sizeof(response));
+    printf("%s", response);
 
     // check allowedclients.txt for pid
     int allowed = 0;
