e9a5b9b4b4d492ad7b65027239a8ba3d0acfb9b2
Martin Ligabue <martinligabue@gmail.com>
2024-06-11 22:57:44 +0200

    aggiunto commento esplicativo

--- inc/common.h
+++ inc/common.h
@@ -3,7 +3,7 @@
 
 #define PORT 12345
 
-#define EXTERNAL 0
+#define EXTERNAL 0 // set this to 1 to connect to an external server
 #if EXTERNAL == 1
 #define ADDRESS "192.168.110.85" // change this to the server address
 #else
@@ -13,7 +13,7 @@
 #define BUFFER_SIZE 1024
 #define MAX_RECORDS 100
 
-#define COMMAND_LIST "Please select the command type:"                               \
+#define COMMAND_LIST "Please select the command type:" \
                      "\n1: Queries the address book\n2: Add a new record (Name,Surname,StudentId)\n3: Edit a record (record to delete, new name, new surname, new studentId)\n4: Delete a record (given the id)\n5: Authenticate with a password\n6: Close this client\nYour selection: "
 
 #define SUBQUERY "Perform the search for: \n1: First Name,\n2: Last Name,\n3: Student ID.\nYour selection: "
